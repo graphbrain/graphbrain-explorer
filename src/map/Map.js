b5803e83e046cf6ccfcc93d865c00dc4db6b6118
Yael Stein <yaelst@gmail.com>
2020-02-14 18:06:26 +0100

    arrange code

--- src/map/Map.js
+++ src/map/Map.js
@@ -10,9 +10,6 @@ import * as data from '../api/conflicts.json';
 class Map extends Component {
   constructor(props) {
     super(props);
-
-    console.log(window.screen.width);
-
     this.state = {
       nodes: data.nodes,
       links: data.links,
@@ -20,20 +17,16 @@ class Map extends Component {
       height: 600,
       types: ["conflict"]
     }
-
-    this.drag = this.drag.bind(this);
-
   }
 
   componentDidMount() {
     const { width, height, types, nodes, links } = this.state;
-
     
     const simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.id))
-    .force("charge", d3.forceManyBody().strength(-400))
-    .force("x", d3.forceX())
-    .force("y", d3.forceY());
+      .force("link", d3.forceLink(links).id(d => d.id))
+      .force("charge", d3.forceManyBody().strength(-400))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
 
     const linkArc = (d) => {
       const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
@@ -44,7 +37,6 @@ class Map extends Component {
     }
 
     const color = d3.scaleOrdinal(types, d3.schemeCategory10);
-
 
     const svg = d3.select("svg")
     .attr("viewBox", [-width / 2 - 50, -height / 2 + 60, width, height])
@@ -60,11 +52,11 @@ class Map extends Component {
       .attr("markerWidth", 5)
       .attr("markerHeight", 5)
       .attr("orient", "auto")
-    .append("path")
+      .append("path")
       .attr("fill", color)
       .attr("d", "M0,-5L10,0L0,5");
 
-      const link = svg.append("g")
+    const link = svg.append("g")
       .attr("fill", "none")
       .attr("stroke-width", 1.5)
       .selectAll("path")
@@ -73,10 +65,7 @@ class Map extends Component {
       .attr("stroke", d => color(d.type))
       .attr('marker-end',d => d.directed ? `url(#arrow-${d.type})` : '')
 
-      link.append("title")
-      .text(d => d.label);
-
-      const linkpaths = svg.selectAll(".linkpath")
+    const linkpaths = svg.selectAll(".linkpath")
       .data(links)
       .enter()
       .append('path')
@@ -86,63 +75,56 @@ class Map extends Component {
       .attr("id", (d, i) => "linkpath" + i)
       .style("pointer-events", "none");
 
-     const linklabels = svg.selectAll(".linklabels")
-          .data(links)
-          .enter()
-          .append('text')
-          .style("pointer-events", "none")
-          .attr("class", "linklabels")
-          .attr("font-size", 4)
-          .attr("fill", "#aaa")
-          .attr("id", (d, i) => "linklabel" + i)
-      
+    const linklabels = svg.selectAll(".linklabels")
+      .data(links)
+      .enter()
+      .append('text')
+      .style("pointer-events", "none")
+      .attr("class", "linklabels")
+      .attr("font-size", 4)
+      .attr("fill", "#aaa")
+      .attr("id", (d, i) => "linklabel" + i)  
 
-          linklabels.append('textPath')
-          .attr('xlink:href', (d, i) => '#linkpath' + i)
-          .style("text-anchor", "middle")
-          .style("pointer-events", "none")
-          .attr("startOffset", "50%")
-          .text(d => d.label);
-
-
-      
+    linklabels.append('textPath')
+      .attr('xlink:href', (d, i) => '#linkpath' + i)
+      .style("text-anchor", "middle")
+      .style("pointer-events", "none")
+      .attr("startOffset", "50%")
+      .text(d => d.label);
 
       const node = svg.append("g")
-      .attr("fill", "currentColor")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-linejoin", "round")
-      .selectAll("g")
-      .data(nodes)
-      .join("g") 
-      .call(this.drag(simulation));
+        .attr("fill", "currentColor")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-linejoin", "round")
+        .selectAll("g")
+        .data(nodes)
+        .join("g") 
+        .call(this.drag(simulation));
 
+      node.append("circle")
+        .attr("stroke", "white")
+        .attr("stroke-width", 1.5)
+        .attr("r", 3);
 
-    node.append("circle")
-    .attr("stroke", "white")
-    .attr("stroke-width", 1.5)
-    .attr("r", 3);
-
-    node.append("text")
-      .attr("x", 8)
-      .attr("y", "0.31em")
-      .text(d => d.label)
-      .clone(true).lower()
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-width", 3);
+      node.append("text")
+        .attr("x", 8)
+        .attr("y", "0.31em")
+        .text(d => d.label)
+        .clone(true).lower()
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-width", 3);
 
       simulation.on("tick", () => {
         link.attr("d", linkArc).attr("class", "linkArc");
         node.attr("transform", d => `translate(${d.x},${d.y})`);
         linkpaths.attr('d', d => 
            `M ${d.source.x} ${d.source.y} L ${d.target.x} ${d.target.y}`
-      );
+        );
       });
-
   }
 
   drag (simulation) {
-  
     function dragstarted(d) {
       if (!d3.event.active) simulation.alphaTarget(0.3).restart();
       d.fx = d.x;
